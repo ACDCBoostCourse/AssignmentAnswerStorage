90f6f1de1b9b59efc67cfbc6e8b93a6955949f04
lee-euijin <leeuj9663@naver.com>
2022-07-01 16:43:22 +0900

    First Commit

---
+++ temp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  temp
+//
+//  Created by 이의진 on 2022/07/01.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
